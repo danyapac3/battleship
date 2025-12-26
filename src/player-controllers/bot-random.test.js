d38725f90c6b44fa56682b68e706d5e35f13bd07
Danylo Dovchenko <danya.pac3@gmail.com>
2025-12-26 04:37:35 +0200

    test(bot): fix test to match async version of getCellPosition

--- src/player-controllers/bot-random.test.js
+++ src/player-controllers/bot-random.test.js
@@ -5,26 +5,28 @@ const controller = new RandomPlayerController();
 const board = new Gameboard();
 
 describe("random-bot getHitPosition method", () => {
-  test("Gives cell within board size", () => {
-    const [x, y] = controller.getCellPosition(board);
+  test("Gives cell within board size", async () => {
+    const [x, y] = await controller.getCellPosition(board);
     expect(x).toBeGreaterThanOrEqual(1);
     expect(x).toBeLessThenOrEqual(10);
     expect(y).toBeGreaterThanOrEqual(1);
     expect(y).toBeLessThenOrEqual(10);
   });
 
-  test("Never picks cell that is already hit", () => {
+  test("Never picks cell that is already hit", async () => {
     // hit all the board except the last cell
     for (let i = 0; i < 99; i++) {
       const x = (i % 10) + 1;
       const y = Math.floor(i / 10) + 1;
       board.hit(x, y);
     }
-    expect(() => controller.getCellPosition()).toStrictEqual([10, 10]);
+    await expect(() => controller.getCellPosition()).resolves.toStrictEqual([
+      10, 10,
+    ]);
   });
 
-  test("Takes full board", () => {
+  test("Takes full board", async () => {
     board.hit(10, 10);
-    expect(() => controller.getCellPosition()).toBe(null);
+    await expect(() => controller.getCellPosition()).resolves.toBe(null);
   });
 });
