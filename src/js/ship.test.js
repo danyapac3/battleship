0ef54c3e86c9744c5819711b2d5f30129544f62b
Danylo Dovchenko <danya.pac3@gmail.com>
2026-01-11 20:55:03 +0200

    test: remove skips from ship.test.js

--- src/js/ship.test.js
+++ src/js/ship.test.js
@@ -1,19 +1,19 @@
 import Ship from "./ship.js";
 
 describe("ship", () => {
-  test.skip("constructs with right length", () => {
+  test("constructs with right length", () => {
     expect(new Ship(4).length).toBe(4);
   });
-  test.skip("throws error if length is not specified", () => {
-    expect(() => void new Ship()).toThrow('Length must be specified')
+  test("throws error if length is not specified", () => {
+    expect(() => void new Ship()).toThrow("Length must be specified");
   });
-  test.skip("Isn't sunk is hit less than its length", () => {
+  test("Isn't sunk is hit less than its length", () => {
     const ship = new Ship(5);
     ship.hit();
     ship.hit();
     expect(ship.isSunk()).toBe(false);
-  })
-  test.skip("It's sunk if it's been hit enough times", () => {
+  });
+  test("It's sunk if it's been hit enough times", () => {
     const ship = new Ship(3);
     ship.hit();
     ship.hit();
